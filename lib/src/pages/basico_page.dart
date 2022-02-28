4e5a99a827e7184d6bbd53b0c4d662a57062c4fc
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-02-28 17:39:05 -0500

    Diseño basico 1

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
+  final estiloTitulo = TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold);
+  final estiloSubTitulo = TextStyle(fontSize: 18.0, color: Colors.grey);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Basico Page'),
-      ),
-    );
+        body: Column(
+      children: <Widget>[
+        Image(
+          image: NetworkImage(
+              'https://mediacloud.theweek.co.uk/image/private/s--oLfrZ_Cj--/v1610376627/theweek/2021/01/cottagecore%20130722-pxhere.jpg'),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Casa en el bosque',
+                      style: estiloTitulo,
+                    ),
+                    SizedBox(height: 4.0),
+                    Text('Ubicado en Amsterdam', style: estiloSubTitulo),
+                  ],
+                ),
+              ),
+              Icon(Icons.star, color: Colors.red, size: 30.0),
+              Text('41', style: TextStyle(fontSize: 20.0))
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
