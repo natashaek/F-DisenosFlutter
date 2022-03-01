903ffff6193b89bc7c9114317808a365339e6472
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-02-28 21:23:01 -0500

    Diseño con scroll 2

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -15,8 +15,47 @@ class ScrollPage extends StatelessWidget {
 }
 
 Widget _pagina1() {
-  return Center(
-    child: Text('Pagina 1'),
+  return Stack(
+    children: <Widget>[
+      _colorFondo(),
+      _imagenFondo(),
+      _textos(),
+    ],
+  );
+}
+
+Widget _imagenFondo() {
+  return Container(
+    width: double.infinity,
+    height: double.infinity,
+    child: Image(
+      image: AssetImage('assets/scroll-1.png'),
+      fit: BoxFit.cover,
+    ),
+  );
+}
+
+Widget _textos() {
+  final estiloTexto = TextStyle(color: Colors.white, fontSize: 50.0);
+
+  return SafeArea(
+    child: Column(
+      children: <Widget>[
+        SizedBox(height: 20.0),
+        Text('11°', style: estiloTexto),
+        Text('Miercoles', style: estiloTexto),
+        Expanded(child: Container()),
+        Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white)
+      ],
+    ),
+  );
+}
+
+Widget _colorFondo() {
+  return Container(
+    width: double.infinity,
+    height: double.infinity,
+    color: Color.fromRGBO(108, 192, 218, 1.0),
   );
 }
 
