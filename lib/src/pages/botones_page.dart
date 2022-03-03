bba44dde6315acf085d1f0f19894383d040ab276
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-03-02 21:23:29 -0500

    Blur y argumentos

--- lib/src/pages/botones_page.dart
+++ lib/src/pages/botones_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
@@ -107,44 +108,50 @@ class BotonesPage extends StatelessWidget {
   Widget _botonesRedondeados() {
     return Table(children: [
       TableRow(children: [
-        _crearBotonRedondeado(),
-        _crearBotonRedondeado(),
+        _crearBotonRedondeado(Colors.blue, Icons.border_all, 'General'),
+        _crearBotonRedondeado(Colors.purpleAccent, Icons.directions_bus, 'Bus'),
       ]),
       TableRow(children: [
-        _crearBotonRedondeado(),
-        _crearBotonRedondeado(),
+        _crearBotonRedondeado(Colors.pinkAccent, Icons.shop, 'Buy'),
+        _crearBotonRedondeado(Colors.orange, Icons.insert_drive_file, 'File'),
       ]),
       TableRow(children: [
-        _crearBotonRedondeado(),
-        _crearBotonRedondeado(),
+        _crearBotonRedondeado(
+            Colors.blueAccent, Icons.movie_filter, 'Entertainment'),
+        _crearBotonRedondeado(Colors.green, Icons.cloud, 'Grocery'),
       ]),
       TableRow(children: [
-        _crearBotonRedondeado(),
-        _crearBotonRedondeado(),
+        _crearBotonRedondeado(Colors.red, Icons.collections, 'Photos'),
+        _crearBotonRedondeado(Colors.teal, Icons.help_outline, 'General'),
       ]),
     ]);
   }
 
-  Widget _crearBotonRedondeado() {
-    return Container(
-      height: 180.0,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: Color.fromRGBO(62, 66, 107, 0.7),
-          borderRadius: BorderRadius.circular(20.0)),
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            SizedBox(height: 5.0),
-            CircleAvatar(
-              backgroundColor: Colors.pinkAccent,
-              radius: 35.0,
-              child: Icon(Icons.swap_calls, color: Colors.white, size: 30.0),
-            ),
-            Text('Cosa',
-                style: TextStyle(color: Color.fromARGB(255, 131, 20, 57))),
-            SizedBox(height: 5.0),
-          ]),
+  Widget _crearBotonRedondeado(Color color, IconData icono, String texto) {
+    return ClipRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+        child: Container(
+          height: 180.0,
+          margin: EdgeInsets.all(15.0),
+          decoration: BoxDecoration(
+              color: Color.fromRGBO(62, 66, 107, 0.7),
+              borderRadius: BorderRadius.circular(20.0)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(height: 5.0),
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35.0,
+                child: Icon(icono, color: Colors.white, size: 30.0),
+              ),
+              Text(texto, style: TextStyle(color: color)),
+              SizedBox(height: 5.0)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
